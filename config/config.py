8379c486fb3a116e8319b73fc395248e23f07310
shenkai <lingyue@yunquna.com>
2021-04-21 12:06:46 +0800

    git commit

---
+++ config/config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author: Kai Shen
+# @Created Time: 2021/4/16 20:02
+# @Organization: YQN
+
+detect_config={
+    "config":[
+        {
+            "open":1,
+            "blur":3,
+            "morphologyr":4,
+            "morphologyc":19,
+            "col_num_limit":10,
+            "row_num_limit":21
+        },
+        {
+            "open":0,
+            "blur":3,
+            "morphologyr":5,
+            "morphologyc":12,
+            "col_num_limit":10,
+            "row_num_limit":18
+        }
+    ]
+}
